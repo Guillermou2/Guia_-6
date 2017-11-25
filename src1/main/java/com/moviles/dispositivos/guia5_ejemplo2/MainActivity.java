2e67fbf47445bdd26de33930f773c6c220511e74
Guillermou2 <32018002+Guillermou2@users.noreply.github.com>
2017-11-24 23:28:54 -0600

    Add files via upload

---
+++ src1/main/java/com/moviles/dispositivos/guia5_ejemplo2/MainActivity.java
@@ -0,0 +1,13 @@
+package com.moviles.dispositivos.guia5_ejemplo2;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
